3ef9b4c406bb19dcd55c0d025dccde5051368d47
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-30 16:41:48 +0200

    added elements to get the post from index and write it in the page

--- server.php
+++ server.php
@@ -1,5 +1,10 @@
 <?php 
 
+$paragraph = $_POST["paragraph"];
+$censor = $_POST["censor"];
+
+$censored = str_replace($censor, '*****', $paragraph)
+
 ?>
 
 <!DOCTYPE html>
@@ -11,5 +16,40 @@
 </head>
 <body>
     
+    <main>
+        <div class="box">
+            <h1>Il paragrafo censurato è:</h1>
+            <h1><?php echo $censored; ?></h1>
+        </div>
+    </main>
+
+</body>
+</html>
+
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    main{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+    .box{
+        height: 50%;
+        aspect-ratio: 1;
+        background-color: lightgrey;
+        border-radius: 20px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+</style>
+
 </body>
 </html>
